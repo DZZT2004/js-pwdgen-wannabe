186637d6aa78cdaf33e34e7d6b8cf998f9a1e8e6
DZZT2004 <taodaiuwu@gmail.com>
2022-10-19 23:42:48 +0200

    Esercizio finito

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Inserisci il nome");
+var cognome = prompt("Inserisci il cognome");
+var colore = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById('password').innerHTML = nome + cognome + colore + '22';
+
+console.log(nome + cognome + colore + '22');
